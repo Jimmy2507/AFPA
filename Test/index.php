6f51e73cb6a4eaaf5c33d28b5b4b2c21f2315702
Jimmy2507 <jimmy.mrchnd@gmail.com>
2021-03-25 11:06:35 +0100

    exo2

--- Test/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require("./Services/methodes.php");
-function chargementClasse($class){
-            require("./class/".$class.".class.php");
-        }
-        spl_autoload_register("chargementClasse");
