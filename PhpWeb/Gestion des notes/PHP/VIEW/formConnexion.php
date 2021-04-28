91820e1070d21089609815894d1040d8beeb314c
Jimmy2507 <jimmy.mrchnd@gmail.com>
2021-04-28 17:30:24 +0200

    eval

--- PhpWeb/Gestion des notes/PHP/VIEW/formConnexion.php
+++ PhpWeb/Gestion des notes/PHP/VIEW/formConnexion.php
@@ -1,7 +1,13 @@
 <form action="?page=actionConnexion" method="POST">
-    <label for="login">Pseudo :</label>
-    <div><input type="text" name ="login"require> </div>
-    <label for="motDePasse">Mot de passe :</label>
-    <div><input type="password" name ="motDePasse"require></div>
-    <div ><button class="Boutton" type="submit">Valider</button></div>
+    <div class="ligne">
+    <div class="EspaceV1"></div>
+        <div class=" Block colonne">
+            <label for="login">Pseudo :</label>
+            <div><input type="text" name ="login"require> </div>
+            <label for="motDePasse">Mot de passe :</label>
+            <div><input type="password" name ="motDePasse"require></div>
+            <div ><button class="Boutton" type="submit">Valider</button></div>
+        </div>
+        <div class="EspaceV1"></div>
+    </div>
 </form>
