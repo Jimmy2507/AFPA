31b243b0eefbf12602203f8b54a09ddae2628182
Jimmy2507 <jimmy.mrchnd@gmail.com>
2021-05-18 12:20:04 +0200

    18/05/2021 12:20:03,15

--- JS/snake/script.js
+++ JS/snake/script.js
@@ -1,20 +1,27 @@
-var nbColonne=30;
-var nbLigne=20;
-var cote=20;
-var score=document.getElementById("score");
-var dessin=document.getElementById("dessin");
-var ctx=dessin.getContext("2d");
-var snake=[[4,0],[3,0],[2,0],[1,0],[0,0]];
-var snakeIncX=1;
-var snakeIncY=0;
+nbColonne=30;
+nbLigne=20;
+cote=20;
+score=document.getElementById("score");
+dessin=document.getElementById("dessin");
+ctx=dessin.getContext("2d");
+listeBouttons = document.getElementsByTagName("button");
+for (let i = 0; i < listeBouttons.length; i++) {
+	listeBouttons[i].addEventListener("click",function(){
+        couleurSnake(i);
+    })
+}
+
+snake=[[1,0]];
+snakeIncX=1;
+snakeIncY=0;
 dessin.width=nbColonne*cote;
 dessin.height=nbLigne*cote;
-ctx.fillStyle="#0000FF";
-var timerJeu=setInterval(boucleJeu,100);
+timerJeu=setInterval(boucleJeu,100);
 placeBonbon();
-
+couleurSnake();
+alert("Appuie sur ok pour commencer ! ")
 window.onkeydown = function(e) {
-    var key = e.keyCode;
+    key = e.keyCode;
 	switch(key){
 		case 37: //Gauche
 			if(snakeIncX==0){
@@ -40,12 +47,27 @@ window.onkeydown = function(e) {
 				snakeIncY=1;
 			}
 		break;
+		case 27:
+			//Echap
+			alert("Pause")
+			break;
+	}
+}
+function couleurSnake(numBtn){
+	if (numBtn == 0) {
+		ctx.fillStyle="red";
+	}else if(numBtn == 1){
+		ctx.fillStyle="blue";
+	}else if(numBtn == 2){
+		ctx.fillStyle="green";
+	}else{
+		ctx.fillStyle="green";
 	}
 }
 
 function majDessin(){
 	ctx.clearRect(0,0,dessin.width,dessin.height);
-	for(var i=0,l=snake.length;i<l;i++){
+	for(i=0,l=snake.length;i<l;i++){
 		ctx.fillRect(snake[i][0]*cote,snake[i][1]*cote,cote,cote);
 	}
 	ctx.fillRect(bonbon[0]*cote,bonbon[1]*cote,cote,cote);	
@@ -75,22 +97,26 @@ function placeBonbon(){
 
 function bougeSnake(){
 
-	var tete=[snake[0][0]+snakeIncX,snake[0][1]+snakeIncY];
-
+	tete=[snake[0][0]+snakeIncX,snake[0][1]+snakeIncY];
+	//SI SNAKE SE PREND LE MUR
     if((tete[0]==-1||tete[0]==nbColonne)||(tete[1]==-1||tete[1]==nbLigne)){
         return false;
     } 
-    for(var i=0,l=snake.length-1;i<l;i++){
+	//SI SNAKE TOUCHE SON COROP
+    for(i=0,l=snake.length-1;i<l;i++){ //BOUCLE POUR PARCOURIR TOUT LES ELEMENT DU CORP ET COMPARE A LA NOUVELLE POSITION DE LA TETE
 		if((tete[0]==snake[i][0])&&(tete[1]==snake[i][1])){
            return false; 
         } 
 	}
     if((tete[0]==bonbon[0])&&(tete[1]==bonbon[1])){
 		placeBonbon();
-		majScore(snake.length-4);
+		majScore(snake.length);
+
 	}else{
 		snake.pop();
-	}
-    snake.unshift(tete);
-	return true;
+	}		
+		snake.unshift(tete);
+		return true;
+	//REMPLACE LA TETE PAR UN NOUVELLE ELEMENT
+
 }
