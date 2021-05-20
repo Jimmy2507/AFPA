b628211f95b5a76bf2ce2489a24da41cfa7db26d
Jimmy2507 <jimmy.mrchnd@gmail.com>
2021-05-20 17:30:41 +0200

    formulaire

--- JS/formulaire/script.js
+++ JS/formulaire/script.js
@@ -1,17 +1,32 @@
 input = document.getElementsByTagName("input");
+oeil = document.querySelector("#oeil");
+oeilBarre = document.querySelector("#oeilBarre");
+oeil.addEventListener("mousedown",afficheMdp);
+oeilBarre.addEventListener("mousedown",Mdp);
 for (let i = 0; i < input.length; i++) {
-    input[i].addEventListener("input",verifInput(i));
+    input[i].addEventListener("input",function(){
+        verifInput(i);
+    });
 }
-function verifInput(k){
-    erreurs = document.getElementsByClassName("erreur")
-    for (let i = 0; i < erreurs.length; i++) {
-        if(k == i){
-            if(input.checkValidity()){
-                erreurs[i].innerHTML ="ok"
-            }else{
-                erreurs[i].innerHTML ="ko"
-            }
-        }
-    }
 
+function verifInput(i){ 
+    if(input[i].checkValidity()){
+        input[i].style.color="green";
+    }else{
+        input[i].style.color="red";
+    }   
+}
+
+function afficheMdp(){
+    var mdp = document.querySelector("#Mdp");
+    mdp.setAttribute("type","text")
+    oeil.style.display = "none"
+    oeilBarre.style.display = "flex"
+}
+
+function Mdp(){
+    var mdp = document.querySelector("#Mdp");
+    mdp.setAttribute("type","password")
+    oeil.style.display = "flex"
+    oeilBarre.style.display = "none"
 }
