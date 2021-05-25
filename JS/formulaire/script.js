9918c975ca995853318eb66e04c15ec93cd48df9
Jimmy2507 <jimmy.mrchnd@gmail.com>
2021-05-25 08:40:25 +0200

    form

--- JS/formulaire/script.js
+++ JS/formulaire/script.js
@@ -1,17 +1,18 @@
 input = document.getElementsByTagName("input");
+
 for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("input",verifInput(i));
 }
+
 function verifInput(k){
     erreurs = document.getElementsByClassName("erreur")
     for (let i = 0; i < erreurs.length; i++) {
         if(k == i){
-            if(input.checkValidity()){
+            if(input[k].checkValidity()){
                 erreurs[i].innerHTML ="ok"
             }else{
                 erreurs[i].innerHTML ="ko"
             }
         }
     }
-
 }
